ad8a2d9e3e1d3b86a9b9c4eee5d14fe92a8cb3d7
Linuxperoxo <linuxperoxo@gmail.com>
2025-04-04 18:28:01 -0300

    update to Atlas configuration struct for kernel boot

--- entry/entry.s
+++ entry/entry.s
@@ -6,11 +6,14 @@
 #    |                                            |
 #    O--------------------------------------------/
 
-.equ ATLASMAGIC, 0xAB00
-.equ ATLASLOAD, 0x1000000
+.equ AtlasMagic, 0xAB00
+.equ AtlasLoadDest, 0x1000000
+.equ AtlasVMode, 0x1000
+.equ AtlasFlags, 0b00000000
+
 .equ STACK, 0x3000000
 
-.extern Smain
+#.extern Smain
 
 .section .opensaturn.text.entry, "ax", @progbits
 .code32
@@ -19,14 +22,15 @@
 Sentry:
   cli
   movl $STACK, %esp
-  call Smain
+  #call Smain
   hlt
 
 .section .opensaturn.atlas.entry, "a", @progbits
 .type .AtlasFlags, @object
-.AtlasFlags:               # NOTE: Esse headers deve ser colocado no início do binário
-  .word ATLASMAGIC         # NOTE: Flag mágica para dizer para o Atlas que é um arquivo válido para boot
-  .long ATLASLOAD          # NOTE: Endereço de memória físico de onde o arquivo deve ser carregado 
-  .long Sentry - ATLASLOAD # NOTE: Offset dentro do arquivo para o onde o Atlas deve passar o controle de execução.
-  .long ATLASSIZE          # NOTE: Tamanho do arquivo em bytes, esse está sendo colocado diretamente no ../linker.ld
-
+.AtlasFlags:                   # NOTE: Esse headers deve ser colocado no início do binário
+  .word AtlasMagic             # NOTE: Flag mágica para dizer para o Atlas que é uma imagem válida para boot
+  .long AtlasLoadDest          # NOTE: Endereço de memória físico de onde a imagem deve ser carregado 
+  .long Sentry - AtlasLoadDest # NOTE: Offset dentro do arquivo para o onde o Atlas deve passar o controle de execução.
+  .long AtlasImgSize           # NOTE: Tamanho do arquivo em bytes, esse está sendo colocado diretamente no ../linker.ld
+  .word AtlasVMode             # NOTE: Modo de vídeo que queremos, VGA 80x25
+  .byte AtlasFlags             # NOTE: Flags gerais para o Atlas, consulte a documentação no código do Atlas https://github.com/Linuxperoxo/AtlasB/blob/master/src/atlas.s
