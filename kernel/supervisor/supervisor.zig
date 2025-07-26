27a61c5e867b2b646e217d607fac19bebf864519
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2025-07-26 18:06:43 -0300

    Supervisor corrections and improvements

--- kernel/supervisor/supervisor.zig
+++ kernel/supervisor/supervisor.zig
@@ -3,42 +3,7 @@
 // │            Author: Linuxperoxo                 │
 // └────────────────────────────────────────────────┘
 
-pub const supervisor_T: type = @import("interfaces.zig").supervisor_T;
-pub const initSupervisor_T: type = @import("interfaces.zig").initSupervisor_T;
+pub const supervisor_T: type = @import("types.zig").supervisor_T;
 
-const handler: type = @import("handler.zig");
-
-pub fn init(comptime ISR: anytype, T: type) T {
-    const isr: @TypeOf(ISR) = ISR;
-    var aloneHandler: T = undefined;
-    for(0..isr.len) |i| {
-        switch(isr[i].type) {
-            .exception => {
-                aloneHandler[i] = &(struct {
-                    pub fn exception() noreturn {
-                        @call(.never_inline, &handler.exception, .{i});
-                        unreachable;
-                    }
-                }.exception);
-            },
-            .syscall => {
-                aloneHandler[i] = &(struct {
-                    pub fn syscall() noreturn {
-                        @call(.never_inline, &handler.syscall, .{i});
-                        unreachable;
-                    }
-                }.syscall);
-            },
-            .irq => {
-                aloneHandler[i] = &(struct {
-                    pub fn irq() noreturn {
-                        @call(.never_inline, &handler.irq, .{i});
-                        unreachable;
-                    }
-                }.irq);
-            },
-            .none => continue,
-        }
-    }
-    return aloneHandler;
-}
+pub const supervisorIsrTable = @import("management.zig").supervisorIsrTable;
+pub const supervisorHandlerPerIsr = @import("management.zig").supervisorHandlerPerIsr;
