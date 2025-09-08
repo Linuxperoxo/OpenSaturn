2cd09019a05b76310110f7e2c4055f72710acb38
Linuxperoxo <linuxperoxo@gmail.com>
2025-09-08 12:43:41 -0300

    ref: saturn import and build.zig

--- lib/saturn/interfaces/interfaces.zig
+++
@@ -1,9 +0,0 @@
-// ┌────────────────────────────────────────────────┐
-// │  (c) 2025 Linuxperoxo  •  FILE: interfaces.zig │
-// │            Author: Linuxperoxo                 │
-// └────────────────────────────────────────────────┘
-
-pub const device: type = @import("devices.zig");
-pub const fs: type = @import("fs.zig");
-pub const module: type = @import("module.zig");
-pub const arch: type = @import("arch.zig");
---
+++ linkers/x86/x86-linker.zig
@@ -0,0 +1,6 @@
+// ┌─────────────────────────────────────────────────┐
+// │  (c) 2025 Linuxperoxo  •  FILE: x86-linker.zig  │
+// │            Author: Linuxperoxo                  │
+// └─────────────────────────────────────────────────┘
+
+pub const __SaturnLinkerString__ = @embedFile("x86-linker.ld");
