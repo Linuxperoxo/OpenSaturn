9c230c85e9b43e39c6015cb6996876ac861b736a
Linuxperoxo <linuxperoxo@gmail.com>
2025-11-04 12:24:57 -0300

    fix allocs for resized and single | fix resize | add test to SBA-SOA

--- lib/saturn/kernel/memory/sba/sba.zig
+++ lib/saturn/kernel/memory/sba/sba.zig
@@ -34,14 +34,14 @@ pub fn buildByteAllocator(
         pub const blocks_reserved = if(personality.resize) ((@sizeOf(Pool_T) + block_size - 1) / block_size) else 0;
         pub const block_size = block orelse default_block_size;
 
-        const Pool_T: type = struct {
+        pub const Pool_T: type = struct {
             bytes: ?[]u8 = null,
             refs: usize = blocks_reserved,
             next: ?usize = null,
-            bitmap: [total_bytes_of_pool / block_size]u1 = r: {
+            bitmap: [pool_bitmap_len]u1 = r: {
                 var map = [_]u1 {
                     0
-                } ** (total_bytes_of_pool / block_size);
+                } ** pool_bitmap_len;
                 if(!personality.resize) break :r map;
                 for(0..blocks_reserved) |i|
                     map[i] = 1;
@@ -55,7 +55,9 @@ pub fn buildByteAllocator(
             } = .{},
             private: Private_T = if(Private_T == void) {} else undefined,
 
-            const Private_T: type = if(builtin.is_test) void else switch(config.arch.options.Target) {
+            pub const pool_bitmap_len = total_bytes_of_pool / block_size;
+
+            pub const Private_T: type = if(builtin.is_test) void else switch(config.arch.options.Target) {
                 .i386 => mm.AllocPage_T,
                 else => void,
             };
@@ -68,6 +70,7 @@ pub fn buildByteAllocator(
             IndexOutBounds,
             UndefinedAction,
             MemoryFrag,
+            ZeroBytes,
         };
 
         fn pool_init(pool: *Pool_T) err_T!void {
@@ -97,17 +100,31 @@ pub fn buildByteAllocator(
         }
 
         fn resize(self: *@This()) err_T!void {
+            const pool_config = opaque {
+                pub fn config(pool: *Pool_T) void {
+                    for(0..blocks_reserved) |i| {
+                        pool.bitmap[i] = 1;
+                    }
+                    for(blocks_reserved..Pool_T.pool_bitmap_len) |i| {
+                        pool.bitmap[i] = 0;
+                    }
+                    pool.refs = blocks_reserved;
+                    pool.next = null;
+                    pool.flags = .{
+                        .full = 0,
+                        .hit = 0,
+                        .parent = 0,
+                        .reserved = 0,
+                    };
+                }
+            }.config;
             const pool: *Pool_T = @ptrCast(@alignCast(&self.top.?.bytes.?[0]));
             try @call(.always_inline, pool_init, .{
                 pool
             });
-            pool.refs = 0;
-            pool.flags = .{
-                .full = 0,
-                .hit = 0,
-                .parent = 0,
-            };
-            pool.next = blocks_reserved;
+            @call(.always_inline, pool_config, .{
+                pool
+            });
             self.top.?.flags.parent = 1;
             self.top = pool;
             if(builtin.is_test and personality.resize) {
@@ -117,7 +134,7 @@ pub fn buildByteAllocator(
 
         inline fn check_blocks_range(pool: *Pool_T, blocks: usize, locale: usize) struct { index: ?usize, result: bool } {
             return r: {
-                if((locale + blocks) >= pool.bitmap.len) break :r .{
+                if((locale + blocks) > pool.bitmap.len) break :r .{
                     .index = null,
                     .result = false,
                 };
@@ -144,12 +161,15 @@ pub fn buildByteAllocator(
 
         inline fn mark_blocks(pool: *Pool_T, index: usize, blocks: usize) err_T!void {
             // total_bytes_of_pool / block_size = bitmap.len
-            if((index + blocks) >= pool.bitmap.len) return err_T.IndexOutBounds;
+            if((index + blocks) > pool.bitmap.len) {
+                while(true) {}
+                return err_T.IndexOutBounds;
+            }
             for(index..(index + blocks)) |i|
                 pool.bitmap[i] = 1;
         }
 
-        fn alloc_to_one_frame(self: *@This(), bytes: usize) err_T![]u8 {
+        fn alloc_sigle_frame(self: *@This(), bytes: usize) err_T![]u8 {
             if(self.root.flags.full == 1) return err_T.OutOfMemory;
             var index: usize = self.root.next orelse 0;
             const blocks_to_alloc: usize = cast_bytes_to_block(bytes);
@@ -165,22 +185,30 @@ pub fn buildByteAllocator(
             return self.root.bytes.?[cast_block_to_byte(index)..cast_block_to_byte(index + blocks_to_alloc)];
         }
 
-        fn alloc_to_resized_frame(self: *@This(), bytes: usize) err_T![]u8 {
-            var current_pool: *Pool_T = self.top.?;
+        fn alloc_resized_frame(self: *@This(), bytes: usize) err_T![]u8 {
+            var current_pool: *Pool_T = r: {
+                if(self.top.?.flags.full == 1) try @call(.never_inline, resize, .{
+                    self
+                });
+                break :r self.top.?;
+            };
             var index: usize = current_pool.next orelse blocks_reserved;
             const blocks_to_alloc: usize = cast_bytes_to_block(bytes);
-            for(index..current_pool.bitmap.len) |_| {
-                const check = check_blocks_range(current_pool, blocks_to_alloc, index);
-                if(check.result) break;
-                if(check.index == null) {
-                    try @call(.never_inline, resize, .{
-                        self
-                    });
-                    current_pool = self.top.?;
-                    index = blocks_reserved;
-                    break;
+            r: {
+                for(index..current_pool.bitmap.len) |_| {
+                    const check = check_blocks_range(current_pool, blocks_to_alloc, index);
+                    if(check.result) break :r {};
+                    if(check.index == null) {
+                        try @call(.never_inline, resize, .{
+                            self
+                        });
+                        current_pool = self.top.?;
+                        index = blocks_reserved;
+                        break;
+                    }
+                    index = check.index.? + 1;
                 }
-                index = check.index.? + 1;
+                unreachable;
             }
             try mark_blocks(current_pool, index, blocks_to_alloc);
             current_pool.refs += blocks_to_alloc;
@@ -190,76 +218,43 @@ pub fn buildByteAllocator(
 
         pub fn alloc(self: *@This(), bytes: usize) err_T![]u8 {
             self.top = self.top orelse &self.root;
+            if(bytes == 0) return err_T.ZeroBytes;
             if(self.root.bytes == null) {
                 @branchHint(.cold);
                 try @call(.never_inline, pool_init, .{
                     &self.root
                 });
             }
-            if(personality.resize) {
-                return @call(.always_inline, alloc_to_resized_frame, .{
+            if(comptime personality.resize) {
+                return @call(.always_inline, alloc_resized_frame, .{
                     self, bytes
                 });
             }
-            return @call(.always_inline, alloc_to_one_frame, .{
+            return @call(.always_inline, alloc_sigle_frame, .{
                 self, bytes
             });
         }
 
-        pub fn free(self: *@This(), ptr: []u8) err_T!void {
+        fn free_resized_frame(self: *@This(), ptr: []u8) err_T!void {
             _ = self;
             _ = ptr;
         }
-    };
-}
 
-test "SBA Alloc Test For Single Frame" {
-    const block_size: usize = 0x10;
-    const SBA_T: type = buildByteAllocator(
-        block_size,
-        .{
-            .resize = false,
+        fn free_single_frame(self: *@This(), ptr: []u8) err_T!void {
+            // comecar por aqui
+            _ = self;
+            _ = ptr;
         }
-    );
-    const TestErr_T: type = error {
-        NonResize,
-        ResizeOutOfTime,
-        BlockAlignMiss,
-    };
-    var sba_allocator: SBA_T = .{};
-    var old_ptr: ?[]u8 = null;
-    for(0..comptime(sba_allocator.root.bitmap.len - 1)) |_| { // FIXME: -1
-        const ptr = try sba_allocator.alloc(1);
-        if(old_ptr != null) {
-            if((@intFromPtr(ptr.ptr) - @intFromPtr(old_ptr.?.ptr)) != SBA_T.block_size) return TestErr_T.BlockAlignMiss;
-        }
-        old_ptr = ptr;
-    }
-}
 
-test "SBA Alloc Test For Resized Frame" {
-    const block_size: usize = 0x10;
-    const SBA_T: type = buildByteAllocator(
-        block_size,
-        .{
-            .resize = true,
+        pub fn free(self: *@This(), ptr: []u8) err_T!void {
+            if(comptime personality.resize) {
+                @call(.always_inline, free_resized_frame, .{
+                    self, ptr
+                });
+            }
+            @call(.always_inline, free_single_frame, .{
+                self, ptr
+            });
         }
-    );
-    const TestErr_T: type = error {
-        NonResize,
-        ResizeOutOfTime,
-        BlockAlignMiss,
     };
-    var sba_allocator: SBA_T = .{};
-    var old_ptr: ?[]u8 = null;
-    for(SBA_T.blocks_reserved..comptime(sba_allocator.root.bitmap.len - SBA_T.blocks_reserved)) |_| {
-        const ptr = try sba_allocator.alloc(1);
-        if(old_ptr != null) {
-            if((@intFromPtr(ptr.ptr) - @intFromPtr(old_ptr.?.ptr)) != SBA_T.block_size) return TestErr_T.BlockAlignMiss;
-        }
-        old_ptr = ptr;
-    }
-    if(sba_allocator.resized) return TestErr_T.ResizeOutOfTime;
-    _ = try sba_allocator.alloc(1);
-    if(!sba_allocator.resized) return TestErr_T.NonResize;
 }
---
+++ lib/saturn/kernel/memory/sba/test.zig
@@ -0,0 +1,110 @@
+// ┌──────────────────────────────────────────────┐
+// │  (c) 2025 Linuxperoxo  •  FILE: test.zig     │
+// │            Author: Linuxperoxo               │
+// └──────────────────────────────────────────────┘
+
+// === Teste Info ===
+//
+// OS: Gentoo Linux x86_64
+// Zig: 0.15.2
+// Tester: Linuxperoxo
+// Status: Pass
+
+const buildByteAllocator = @import("sba.zig").buildByteAllocator;
+
+const TestSingleErr_T: type = error {
+    BlockAlignMiss,
+    NonFullAlloc,
+};
+
+const TestResizedErr_T: type = TestSingleErr_T || error {
+    NonResize,
+    ResizeOutOfTime,
+};
+
+test "SBA Alloc Test For Single Frame" {
+    const block_size: usize = 0x10;
+    const SBA_T: type = buildByteAllocator(
+        block_size,
+        .{
+            .resize = false,
+        }
+    );
+    var sba_allocator: SBA_T = .{};
+    var old_ptr: ?[]u8 = null;
+    for(0..SBA_T.Pool_T.pool_bitmap_len) |_| {
+        const ptr = try sba_allocator.alloc(1);
+        if(old_ptr != null) {
+            if((@intFromPtr(ptr.ptr) - @intFromPtr(old_ptr.?.ptr)) != SBA_T.block_size) return TestSingleErr_T.BlockAlignMiss;
+        }
+        old_ptr = ptr;
+    }
+    for(0..SBA_T.Pool_T.pool_bitmap_len) |i| {
+        if(sba_allocator.root.bitmap[i] == 0) return TestSingleErr_T.NonFullAlloc;
+    }
+}
+
+test "SBA Free Test For Single Frame" {
+    const block_size: usize = 0x10;
+    const SBA_T: type = buildByteAllocator(
+        block_size,
+        .{
+            .resize = false,
+        }
+    );
+    var sba_allocator: SBA_T = .{};
+    for(0..SBA_T.Pool_T.pool_bitmap_len) |_| {
+        _ = try sba_allocator.alloc(1);
+    }
+}
+
+test "SBA Alloc Test For Resized Frame" {
+    const block_size: usize = 0x10;
+    const SBA_T: type = buildByteAllocator(
+        block_size,
+        .{
+            .resize = true,
+        }
+    );
+    var sba_allocator: SBA_T = .{};
+    var old_ptr: ?[]u8 = null;
+    for(SBA_T.blocks_reserved..SBA_T.Pool_T.pool_bitmap_len) |_| {
+        const ptr = try sba_allocator.alloc(1);
+        if(old_ptr != null) {
+            if((@intFromPtr(ptr.ptr) - @intFromPtr(old_ptr.?.ptr)) != SBA_T.block_size) return TestResizedErr_T.BlockAlignMiss;
+        }
+        old_ptr = ptr;
+    }
+    for(0..SBA_T.Pool_T.pool_bitmap_len) |i| {
+        if(sba_allocator.root.bitmap[i] == 0) return TestResizedErr_T.NonFullAlloc;
+    }
+    // resized test
+    if(sba_allocator.resized) return TestResizedErr_T.ResizeOutOfTime;
+    _ = try sba_allocator.alloc(1);
+    if(!sba_allocator.resized) return TestResizedErr_T.NonResize;
+    old_ptr = null;
+    for(SBA_T.blocks_reserved..SBA_T.Pool_T.pool_bitmap_len - 1) |_| {
+        const ptr = try sba_allocator.alloc(1);
+        if(old_ptr != null) {
+            if((@intFromPtr(ptr.ptr) - @intFromPtr(old_ptr.?.ptr)) != SBA_T.block_size) return TestResizedErr_T.BlockAlignMiss;
+        }
+        old_ptr = ptr;
+    }
+    for(0..SBA_T.Pool_T.pool_bitmap_len) |i| {
+        if(sba_allocator.top.?.bitmap[i] == 0) return TestResizedErr_T.NonFullAlloc;
+    }
+}
+
+test "SBA Free Test For Resized Frame" {
+    const block_size: usize = 0x10;
+    const SBA_T: type = buildByteAllocator(
+        block_size,
+        .{
+            .resize = true,
+        }
+    );
+    var sba_allocator: SBA_T = .{};
+    for(SBA_T.blocks_reserved..SBA_T.Pool_T.pool_bitmap_len) |_| {
+        _ = try sba_allocator.alloc(1);
+    }
+}
