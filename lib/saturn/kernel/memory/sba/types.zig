34889c1e8da6bf91a27924b29e86311453f2c284
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2025-11-06 12:46:46 -0300

    saturn byte allocator

---
+++ lib/saturn/kernel/memory/sba/types.zig
@@ -0,0 +1,22 @@
+// ┌──────────────────────────────────────────────┐
+// │  (c) 2025 Linuxperoxo  •  FILE: types.zig    │
+// │            Author: Linuxperoxo               │
+// └──────────────────────────────────────────────┘
+
+const mm: type = @import("root").mm;
+const config: type = @import("root").config;
+
+pub const Personality_T: type = struct {
+    resize: bool = true,
+    resizeErr: bool = false,
+};
+
+//pub const Cache_T: type = struct {
+//    size: CacheSize_T = .auto,
+//
+//    pub const CacheSize_T: type = enum(u3) {
+//        small = 4,
+//        large = 2,
+//        huge = 1,
+//    };
+//};
