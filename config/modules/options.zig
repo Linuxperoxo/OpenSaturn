20b0e57ae0e68da540c8e977c689d101e9cfcc15
Linuxperoxo <linuxperoxo@gmail.com>
2025-11-21 21:59:08 -0300

    module config file

--- config/modules/options.zig
+++ config/modules/options.zig
@@ -3,5 +3,21 @@
 // │            Author: Linuxperoxo               │
 // └──────────────────────────────────────────────┘
 
+// * UseMenuconfigAsRef: Usa o mod.conf.zig como referencia para
+//                       habilitar o modulo ou nao. Caso desativado
+//                       carrega todos os modulos
+//
+//
+// * IgnoreModuleWithArchNotSupported: Caso um modulo que nao seja
+//                                     suportado pela arquitetura target,
+//                                     vai dar um erro de compilacao. Caso
+//                                     desativado apenas e ignorado
+//
+// * AllowDynamicModulesLoad: Permite carregar modulos de forma dinamica (not working)
+//                            Modulos dinamicos seram carregados de forma separada dos
+//                            modulos linkados ao kernel direto na compilacao, isso e
+//                            importante para evitar possiveis problemas de seguranca
+
 pub const UseMenuconfigAsRef: bool = true;
 pub const IgnoreModuleWithArchNotSupported: bool = true;
+pub const AllowDynamicModulesLoad: bool = true; // TODO:
