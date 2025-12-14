9a18a7712302ca410cbc77d7f29aef9250a2b6fe
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2025-12-14 18:35:15 -0300

    [OpenSaturn 0.2.1]

--- kernel/core/module/main.zig
+++ kernel/core/module/main.zig
@@ -9,6 +9,7 @@ const ModErr_T: type = @import("types.zig").ModErr_T;
 const ModHandler_T: type = @import("types.zig").ModHandler_T;
 const ModRoot_T: type = @import("types.zig").ModRoot_T;
 
+const aux: type = @import("aux.zig");
 const builtin: type = @import("builtin");
 const allocator: type = @import("allocator.zig");
 const mem: type = if(!builtin.is_test) @import("root").kernel.utils.mem else @import("test/mem.zig");
@@ -19,7 +20,7 @@ const fs: type = @import("root").interfaces.fs;
 // assim do jeito que esta ja que sao poucos
 // tipos de modulos
 
-const handlers = [_]ModHandler_T {
+pub const handlers = [_]ModHandler_T {
     .{
         .filesystem = if(builtin.is_test) {} else .{
             .install = fs.registerfs,
@@ -29,11 +30,10 @@ const handlers = [_]ModHandler_T {
 };
 
 // nenhuma das outras partes do kernel tem obrigacao de salvar
-// os modulos do seu tipo, caso por exemplo, o fs queira procurar
-// o certo fs, ele vai chamar o srchmod, isso deixa o kernel em geral
-// mais limpo
+// os modulos do seu tipo, mas podem sim querer salvar e deixar
+// o modulo como anon
 
-var modules_entries = [_]ModRoot_T {
+pub var modules_entries = [_]ModRoot_T {
     .{
         .list = .{},
         .type = .filesystem,
@@ -49,126 +49,76 @@ pub const test_fn = if(!builtin.is_test) @compileError("only in tests") else opa
     }
 };
 
-inline fn module_root_entry(mod_type: ModType_T) *ModRoot_T {
-    for(&modules_entries) |*entry| {
-        if(entry.type == mod_type) return entry;
-    }
-    unreachable;
-}
-
-inline fn find_handler(mod_type: ModType_T) *const ModHandler_T {
-    for(&handlers) |*handler| {
-        switch(handler.*) {
-            .filesystem => if(mod_type == .filesystem) return handler else continue,
-            else => unreachable,
-        }
-    }
-    unreachable;
-}
-
-inline fn resolve_mod_type(mod: *const Mod_T) ModType_T {
-    return switch(mod.private) {
-        .filesystem => ModType_T.filesystem,
-        else => unreachable,
-    };
-}
-
-inline fn calling_handler(mod: *const Mod_T, comptime op: enum { install, remove }) ModErr_T!void {
-    const handler: *const ModHandler_T = find_handler(
-        resolve_mod_type(mod)
-    );
-    switch(handler.*) {
-        .filesystem => |f| {
-            switch(@typeInfo(@TypeOf(f))) {
-                .void => return,
-                else => {},
-            }
-            if(@field(f, @tagName(op)) != null) {
-                @call(.never_inline, @field(f, @tagName(op)).?, .{
-                    &mod.private.filesystem
-                }) catch return ModErr_T.SectionHandlerError;
-            }
-        },
-        else => unreachable,
-    }
-}
-
 /// * search module by name and type
 pub fn srchmod(name: []const u8, mod_type: ModType_T) ModErr_T!*const Mod_T {
-    const module_root: *ModRoot_T = module_root_entry(
+    const module_root: *ModRoot_T = aux.module_root_entry(
         mod_type
     );
     if(module_root.flags.init == 0) return ModErr_T.NoNFound;
-    const iterator_handler_param: struct { find: []const u8 } = .{
-        .find = name,
-    };
-    return module_root.list.iterator_handler(
-        iterator_handler_param,
-        &opaque {
-            pub fn handler(
-                module: *const Mod_T,
-                struct_param: @TypeOf(iterator_handler_param),
-            ) anyerror!void {
-                return if(!mem.eql(module.name, struct_param.find, .{ .case = true })) error.NoNFoundContinue
-                    else {};
-            }
-        }.handler,
-    ) catch |err| return switch(err) {
-        @TypeOf(module_root.list).ListErr_T.EndOfIterator => ModErr_T.NoNFound,
-        else => ModErr_T.IteratorFailed,
-    };
+    if(aux.search_by_module(module_root, null, name)) |mod_found| {
+        const module, _ = mod_found;
+        if(module.flags.control.anon == 1)
+            return ModErr_T.NoNFound;
+        return module;
+    } else |err| {
+        return err;
+    }
 }
 
 /// * install module
-pub fn inmod(mod: *const Mod_T) ModErr_T!void {
-    const module_root: *ModRoot_T = module_root_entry(
-        resolve_mod_type(mod)
+pub fn inmod(mod: *Mod_T) ModErr_T!void {
+    const module_root: *ModRoot_T = aux.module_root_entry(
+        aux.resolve_mod_type(mod)
     );
     module_root.flags.init = if(module_root.flags.init == 1) module_root.flags.init else r: {
         module_root.list.init(&allocator.sba.allocator)
             catch return ModErr_T.ListInitFailed;
         break :r 1;
     };
-    module_root.list.push_in_list(
-        &allocator.sba.allocator,
-        mod
-    ) catch return ModErr_T.ListOperationError;
-    calling_handler(mod, .install) catch {
-        @branchHint(.unlikely);
-        module_root.list.drop_on_list(
-            // last vai retornar justamente o modulo que acabou de
-            // ser adicionado a lista
-            module_root.list.last_index() catch unreachable,
-            &allocator.sba.allocator,
-        ) catch return ModErr_T.ListOperationError;
-    };
+    if(aux.search_by_module(module_root, mod, mod.name)) |mod_found| {
+        const module, const collision = mod_found;
+        @as(*u2, @alignCast(@ptrCast(&module.flags.internal.collision))).* = @as(u2, @intCast(@intFromEnum(collision)));
+        return ModErr_T.ModuleCollision;
+    } else |err| switch(err) {
+        ModErr_T.NoNFound => {},
+        else => return err,
+    }
+    module_root.list.push_in_list(&allocator.sba.allocator, mod)
+        catch return ModErr_T.ListOperationError;
+    mod.flags.internal.installed = 1;
+    mod.flags.internal.removed = 0;
+    if(mod.flags.control.call.init == 1) {
+        mod.flags.internal.call.init = 1;
+        mod.init() catch {
+            mod.flags.internal.fault.call.init = 1;
+            return;
+        };
+    }
+    if(mod.flags.control.call.after == 1) {
+        if(mod.after == null) {
+            mod.flags.internal.call.after = 0;
+            mod.flags.internal.fault.call.after = 1;
+            return;
+        }
+        mod.flags.internal.call.after = 1;
+        mod.after.?() catch {
+            mod.flags.internal.fault.call.after = 1;
+        };
+    }
 }
 
 /// * remove module
-pub fn rmmod(mod: *const Mod_T) ModErr_T!void {
-    const module_root: *ModRoot_T = module_root_entry(
-        resolve_mod_type(mod)
+pub fn rmmod(mod: *Mod_T) ModErr_T!void {
+    const module_root: *ModRoot_T = aux.module_root_entry(
+        aux.resolve_mod_type(mod)
     );
-    const iterator_handler_param: struct { find: *const Mod_T } = .{
-        .find = mod,
-    };
     // esse iterator serve para colocar o index do iterator exatamente
     // no modulo que queremos
-    _ = module_root.list.iterator_handler(
-        iterator_handler_param,
-        &opaque {
-            pub fn handler(
-                module: *const Mod_T,
-                struct_param: @TypeOf(iterator_handler_param)
-            ) anyerror!void {
-                return if(struct_param.find != module) error.NoNFoundContinue
-                    else {};
-            }
-        }.handler,
-    ) catch |err| return switch(err) {
-        @TypeOf(module_root.list).ListErr_T.EndOfIterator => ModErr_T.NoNFound,
-        else => ModErr_T.IteratorFailed,
-    };
+    _ = try aux.search_by_module(module_root, mod, null);
+    if(mod.flags.control.call.remove == 0) {
+        mod.flags.internal.fault.remove = 1;
+        return ModErr_T.OperationDenied;
+    }
     module_root.list.drop_on_list(
         // o index do iterator - 1 vai estar exatamente no modulo
         // que queremos
@@ -176,8 +126,16 @@ pub fn rmmod(mod: *const Mod_T) ModErr_T!void {
         &allocator.sba.allocator,
     ) catch return ModErr_T.AllocatorError; // aqui so pode dar erro do alocador
     module_root.flags.init = @intFromBool(module_root.list.how_many_nodes() > 0);
-    calling_handler(mod, .remove) catch {
+    aux.calling_handler(mod, .remove) catch {
         @branchHint(.unlikely);
         return ModErr_T.RemovedButWithHandlerError;
     };
+    mod.flags.internal.installed = 0;
+    mod.flags.internal.removed = 1;
+    if(mod.flags.control.call.exit == 1) {
+        mod.flags.internal.call.exit = 1;
+        mod.exit() catch {
+            mod.flags.internal.fault.call.exit = 1;
+        };
+    }
 }
