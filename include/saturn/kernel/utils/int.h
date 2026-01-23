6bf374d9422078d0eedc821d61a1bb0b862fbcc6
Linuxperoxo <linuxperoxo@gmail.com>
2026-01-23 12:17:52 -0300

    int c header

---
+++ include/saturn/kernel/utils/int.h
@@ -0,0 +1,35 @@
+// ┌────────────────────────────────────────────┐
+// │  (c) 2026 Linuxperoxo  •  FILE: int.h      │
+// │            Author: Linuxperoxo             │
+// └────────────────────────────────────────────┘
+
+#ifndef INT_H
+#define INT_H
+
+typedef __UINTPTR_TYPE__ uptr;
+typedef __INTPTR_TYPE__ iptr;
+
+typedef __SIZE_TYPE__ usize;
+typedef __PTRDIFF_TYPE__ isize;
+
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+typedef signed long long i64;
+typedef signed int i32;
+typedef signed short i16;
+typedef signed char i8;
+
+typedef u64 uint64_t;
+typedef u32 uint32_t;
+typedef u16 uint16_t;
+typedef u8 uint8_t;
+
+typedef i64 int64_t;
+typedef i32 int32_t;
+typedef i16 int16_t;
+typedef i8 int8_t;
+
+#endif // !INT_H
